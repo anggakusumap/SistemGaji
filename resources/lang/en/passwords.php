1858fa8e6fdb5e6fe3caa90f6ea30e4ddf2a4dd4
anggakusumap <kusuma_putra@student.unud.ac.id>
2021-10-31 11:37:39 +0800

    Update passwords.php

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'reset' => 'Kata sandi Anda telah disetel ulang!',
-    'sent' => 'Kami telah mengirim email tautan reset kata sandi Anda, periksa folder Inbox atau Spam.',
+    'sent' => 'Kami telah mengirim email tautan reset kata sandi Anda, periksa folder Inbox atau Spam Anda.',
     'throttled' => 'Please wait before retrying.',
     'token' => 'Token pengaturan ulang kata sandi ini tidak valid.',
     'user' => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
